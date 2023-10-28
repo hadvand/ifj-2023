9e91247656d2738cc36370c8fc75f6ccecbe54b3
SyruSTR <dargonoed25@mail.ru>
2023-10-28 13:55:34 +0200

    add base struct for parser

---
+++ parser/parser.h
@@ -0,0 +1,19 @@
+//
+// Created by dargo on 28.10.2023.
+//
+
+#ifndef IFJ_2023_PARSER_H
+#define IFJ_2023_PARSER_H
+
+#include "../scanner/scanner.h"
+
+typedef struct Data{
+    //global tab_sym
+    //local tab_sym
+
+    //stack for local
+
+    token_t_ptr token_ptr;
+}TData;
+
+#endif //IFJ_2023_PARSER_H
